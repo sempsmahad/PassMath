16009d1fbb231bbfb0bb3068ed0d49fcbf76efc0
Doozy <dozdoozie@gmail.com>
2022-02-05 08:01:20 +0300

    allow for changing debug domains

--- app/src/main/java/com/kh69/passmath/remote/APIUtils.java
+++ app/src/main/java/com/kh69/passmath/remote/APIUtils.java
@@ -4,7 +4,7 @@ public class APIUtils {
     private APIUtils(){
     };
 
-    public static final String API_URL = "http://192.168.1.101:5000/api/v1/";
+    public static final String API_URL = "http://192.168.43.134:5000/api/v1/";
 
     public static QuestionService getQuestionService(){
         return RetrofitClient.getClient(API_URL).create(QuestionService.class);
