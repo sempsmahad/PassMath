5d0f1f7f63048f42ff91cb25b361e69459fe2082
Doozy <dozdoozie@gmail.com>
2022-02-11 11:55:04 +0300

    added room and kapt

--- app/src/main/java/com/kh69/passmath/data/cache/QuestionDAO.kt
+++ app/src/main/java/com/kh69/passmath/data/cache/QuestionDAO.kt
@@ -1,4 +1,21 @@
 package com.kh69.passmath.data.cache
 
-class QuestionDAO {
+import androidx.lifecycle.LiveData
+import retrofit2.http.Query
+import java.util.*
+
+@Dao
+interface QuestionDAO {
+
+    @Query("SELECT * FROM pattern")
+    fun getPatterns(): LiveData<List<Pattern>>
+
+    @Query("SELECT * FROM pattern WHERE id=(:id)")
+    fun getPattern(id: UUID): LiveData<Pattern?>
+
+    @Update
+    fun updatePattern(pattern: Pattern)
+
+    @Insert
+    fun addPattern(pattern: Pattern)
 }
