93832a7b4dd092c82891af7ce07486948ea06f1a
Doozy <dozdoozie@gmail.com>
2022-02-11 18:07:12 +0300

    question list view model created

---
+++ app/src/main/java/com/kh69/passmath/ui/QuestionListViewModel.kt
@@ -0,0 +1,15 @@
+package com.kh69.passmath.ui
+
+import androidx.lifecycle.ViewModel
+import com.kh69.passmath.data.cache.Question
+import com.kh69.passmath.data.cache.QuestionRepository
+
+
+class QuestionListViewModel : ViewModel() {
+
+    fun addQuestion(question: Question) {
+        questionRepository.addQuestion(question)
+    }
+    private val questionRepository = QuestionRepository.get()
+    val questionListLiveData = questionRepository.getQuestions()
+}
