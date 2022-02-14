94b00f6a0bdaf70fed790c7bad999c5c0a398cae
Doozy <dozdoozie@gmail.com>
2022-02-14 23:52:43 +0300

    api constants

---
+++ app/src/main/java/com/kh69/passmath/data/api/ApiConstants.kt
@@ -0,0 +1,32 @@
+package com.kh69.passmath.data.api
+
+object ApiConstants {
+    const val BASE_ENDPOINT = "https://api.petfinder.com/v2/"
+    const val AUTH_ENDPOINT = "oauth2/token/"
+    const val ANIMALS_ENDPOINT = "animals"
+
+    const val KEY = "INSERT_YOUR_KEY_HERE"
+    const val SECRET = "INSERT_YOUR_SECRET_HERE"
+//    fun aK(): String { //API KEY
+//        return SECRET.substring(35).reversed() + GO.f1 + "5687" + GO.f2 + "3657"+
+//                GO.f2 + SECRET.substring(7,10) + GO.f1 + " 4fj6" + GO.f3 +
+//                SECRET.substring(1..4) + GO.f3
+//    }
+}
+
+object ApiParameters {
+    const val TOKEN_TYPE = "Bearer "
+    const val AUTH_HEADER = "Authorization"
+    const val GRANT_TYPE_KEY = "grant_type"
+    const val GRANT_TYPE_VALUE = "client_credentials"
+    const val CLIENT_ID = "client_id"
+    const val CLIENT_SECRET = "client_secret"
+
+    const val PAGE = "page"
+    const val LIMIT = "limit"
+    const val LOCATION = "location"
+    const val DISTANCE = "distance"
+    const val NAME = "name"
+    const val AGE = "age"
+    const val TYPE = "type"
+}
