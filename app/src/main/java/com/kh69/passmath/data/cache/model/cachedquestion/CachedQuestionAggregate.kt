80451b25201a39ffefb2ed1c457989f5d8862258
Doozy <dozdoozie@gmail.com>
2022-02-17 04:30:22 +0300

    cached question aggregate cleaned

--- app/src/main/java/com/kh69/passmath/data/cache/model/cachedquestion/CachedQuestionAggregate.kt
+++ app/src/main/java/com/kh69/passmath/data/cache/model/cachedquestion/CachedQuestionAggregate.kt
@@ -1,42 +1,18 @@
 package com.kh69.passmath.data.cache.model.cachedquestion
 
 import androidx.room.Embedded
-import androidx.room.Junction
-import androidx.room.Relation
+import com.kh69.passmath.data.cache.Question
 
 
 data class CachedQuestionAggregate(
     @Embedded
-    val animal: CachedAnimalWithDetails,
-    @Relation(
-        parentColumn = "animalId",
-        entityColumn = "animalId"
-    )
-    val photos: List<CachedPhoto>,
-    @Relation(
-        parentColumn = "animalId",
-        entityColumn = "animalId"
-    )
-    val videos: List<CachedVideo>,
-    @Relation(
-        parentColumn = "animalId",
-        entityColumn = "tag",
-        associateBy = Junction(CachedAnimalTagCrossRef::class)
-    )
-    val tags: List<CachedTag>
+    val question: CachedQuestion
 ) {
 
     companion object {
-        fun fromDomain(animalWithDetails: AnimalWithDetails): CachedAnimalAggregate {
-            return CachedAnimalAggregate(
-                animal = CachedAnimalWithDetails.fromDomain(animalWithDetails),
-                photos = animalWithDetails.media.photos.map {
-                    CachedPhoto.fromDomain(animalWithDetails.id, it)
-                },
-                videos = animalWithDetails.media.videos.map {
-                    CachedVideo.fromDomain(animalWithDetails.id, it)
-                },
-                tags =  animalWithDetails.tags.map { CachedTag(it) }
+        fun fromDomain(question: Question): CachedQuestionAggregate {
+            return CachedQuestionAggregate(
+                question = CachedQuestion.fromDomain(question)
             )
         }
     }
