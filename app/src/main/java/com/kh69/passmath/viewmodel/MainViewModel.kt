83aee99959aab495753996126758f5e83d8732b1
Doozy <dozdoozie@gmail.com>
2022-02-27 15:54:03 +0300

    quiz repo

--- app/src/main/java/com/kh69/passmath/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/kh69/passmath/viewmodel/MainViewModel.kt
@@ -1,7 +1,9 @@
 package com.kh69.passmath.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainViewModel(private val repository: QuizRepository) :
     ViewModel() {
