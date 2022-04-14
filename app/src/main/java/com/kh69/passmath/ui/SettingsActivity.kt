5a50da6220b7c257a79df3c1b5a952021090ce26
muhammad <mhdsemps@gmail.com>
2022-04-14 16:14:24 +0300

    Feat/settings page (#32)
    * settings UI and launch action
    
    * [feat] - dummy settings page connected

---
+++ app/src/main/java/com/kh69/passmath/ui/SettingsActivity.kt
@@ -0,0 +1,44 @@
+package com.kh69.passmath.ui
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import com.kh69.passmath.R
+import com.kh69.passmath.Tools2
+
+
+class SettingsActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+        initToolbar()
+    }
+
+
+    private fun initToolbar() {
+        val toolbar = findViewById<View>(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+        supportActionBar!!.title = "Settings"
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        Tools2.setSystemBarColor(this)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_search, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.home) {
+            finish()
+        } else {
+            Toast.makeText(applicationContext, item.title, Toast.LENGTH_SHORT).show()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
