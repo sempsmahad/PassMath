4b178f582ee107e8f29b57bd77504edac49795f6
muhammad <mhdsemps@gmail.com>
2022-08-29 15:09:58 +0300

    attempt last

--- app/src/main/java/com/kh69/passmath/common/data/cache/daos/QuestionsDao.kt
+++ app/src/main/java/com/kh69/passmath/common/data/cache/daos/QuestionsDao.kt
@@ -2,14 +2,13 @@ package com.kh69.passmath.common.data.cache.daos
 
 import androidx.room.*
 import com.kh69.passmath.common.data.cache.model.cachedquestion.CachedQuestion
-import com.kh69.passmath.common.domain.model.question.Question
 import io.reactivex.Flowable
 
 @Dao
 abstract class QuestionsDao {
 
     @Transaction
-    @Query("SELECT * FROM questions ORDER BY questionId DESC")
+    @Query("SELECT * FROM questions")
     abstract fun getAllQuestions(): Flowable<List<CachedQuestion>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
