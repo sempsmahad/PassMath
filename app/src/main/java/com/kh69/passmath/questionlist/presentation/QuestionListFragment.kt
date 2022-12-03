6ce1d34b62405c0e583d17489e1f7842bcd5be27
muhammad <mhdsemps@gmail.com>
2022-12-03 21:48:27 +0300

    planning

--- app/src/main/java/com/kh69/passmath/questionlist/presentation/QuestionListFragment.kt
+++ app/src/main/java/com/kh69/passmath/questionlist/presentation/QuestionListFragment.kt
@@ -1,18 +1,21 @@
-package com.kh69.passmath.quiz.presentation
+package com.kh69.passmath.questionlist.presentation
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.kh69.passmath.databinding.FragmentDashboardBinding
 import com.kh69.passmath.databinding.FragmentQuizBinding
 
-class QuizFragment : Fragment() {
+class QuestionListFragment : Fragment() {
     private val binding get() = _binding!!
     private var _binding: FragmentQuizBinding? = null
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         _binding = FragmentQuizBinding.inflate(inflater, container, false)
 
         return binding.root
@@ -23,5 +26,14 @@ class QuizFragment : Fragment() {
 
 //        setupUI()
 //        requestInitialAnimalsList()
+
+        // if 30 questions are available begin reading and displaying them_____1
+        // else test net connection___2
+        // if available fetch from remote else display error____3
+        // else if result ok save to db____4
+        // else display error from server____5
+        // if all the above were successful start reading else display issue and retry____6
+
+
     }
 }
